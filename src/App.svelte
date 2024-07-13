f20fb841f2bee152f3d346c0fa8a9cfef2b8a12b
Eru <muhamadgilman999@gmail.com>
2024-07-13 18:21:43 +0700

    init: git

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import Navbar from './lib/components/navbar.svelte';
+</script>
+<link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+<main>
+  <Navbar />
+  <div class="hero">
+    <img src={svelteLogo} alt="Svelte logo" width=100 />
+    <h1>Hello Svelte</h1>
+    <p>
+      Testing Svelte Cihuyyyy
+    </p>
+  </div>
+
+</main>
---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import svelteLogo from '../../assets/svelte.svg';
+</script>
+
+<nav>
+    <span class="logo">
+        <img src={svelteLogo} alt="Svelte logo" height=60 width=60/>
+        <p>Svelte</p>
+    </span>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+    </ul>
+</nav>
+
+<style>
+    .logo {
+        display: flex;
+        align-items: center;
+    }
+
+    .logo p{
+        font-size: 42px;
+    }
+    nav {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        background-color: #333;
+        color: white;
+        padding: 2rem;
+    }
+
+    ul {
+        display: flex;
+        list-style: none;
+        gap: 2rem;
+
+    }
+
+    a {
+        color: white;
+        text-decoration: none;
+        font-size: 24px;
+    }
+</style>
